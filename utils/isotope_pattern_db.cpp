e37b567a0036066107c52f999a6174b18beb92d8
Artem Tarasov <artem.tarasov@embl.de>
2016-04-12 19:09:19 +0200

    save isotope computation time to db

--- utils/isotope_pattern_db.cpp
+++ utils/isotope_pattern_db.cpp
@@ -8,6 +8,7 @@ extern "C" {
 }
 
 #include <cstdio>
+#include <chrono>
 #include <mutex>
 
 namespace utils {
@@ -22,7 +23,9 @@ void IsotopePatternDB::computeIsotopePatterns(const utils::InstrumentProfile& in
   if (use_progressbar_)
     bar = progressbar_new("", pairs_.size() / BAR_STEP);
 
-#pragma omp parallel for
+  std::chrono::high_resolution_clock clock;
+
+#pragma omp parallel for private(clock)
   for (size_t i = 0; i < pairs_.size(); i++) {
     const auto& f = pairs_[i].first;
     auto adduct = pairs_[i].second;
@@ -32,13 +35,23 @@ void IsotopePatternDB::computeIsotopePatterns(const utils::InstrumentProfile& in
     auto full_formula = f + adduct;
     try {
       auto res = instrument.resolutionAt(ms::monoisotopicMass(full_formula));
-      auto pattern = ms::computeIsotopePattern(full_formula)\
-        .centroids(res).charged(charge).trimmed(max_peaks);
+
+      auto t1 = clock.now();
+      auto pattern = ms::computeIsotopePattern(full_formula);
+
+      auto t2 = clock.now();
+      pattern = pattern.centroids(res).charged(charge).trimmed(max_peaks);
+
+      auto t3 = clock.now();
       auto key = std::make_pair(f, adduct);
 
       std::lock_guard<std::mutex> lock(map_mutex);
       patterns_[key]["mzs"] = pattern.masses;
       patterns_[key]["abundances"] = pattern.abundances;
+      patterns_[key]["time"] = std::vector<double>{
+        std::chrono::duration<double, std::milli>(t2 - t1).count(),
+        std::chrono::duration<double, std::milli>(t3 - t2).count()
+      };
     } catch (sf_parser::NegativeTotalError) {
       // ignore formulas for which adduct subtraction is impossible
     }
